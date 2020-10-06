bbfae810ee1b5acc643b281c319fda32b0fce568
ariel <ariel@murmuration.ai>
2020-10-06 14:26:35 -0700

    Delete remaining JWT code

--- app/routes/auth.js
+++
@@ -1,7 +0,0 @@
-// let express = require('express');
-// let auth = express.Router();
-// let token = require('../middleware/generate');
-
-// auth.post('/', token.generateAccessToken)
-
-// module.exports = auth;
