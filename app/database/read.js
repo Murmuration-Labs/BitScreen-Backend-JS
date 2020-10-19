763ae96b4ecc8713380952c5c27628db8840cd60
ariel <ariel@murmuration.ai>
2020-10-19 12:45:47 -0700

    Formatting tweaks

--- app/database/read.js
+++ app/database/read.js
@@ -3,11 +3,11 @@ const dynamoClient = require('../config/dynamoDB.config');
 
 const getItem = async (getParams) => {
     const item = dynamoClient.get(getParams, (err, data) => {
-    if (err) {
-        console.log(`Error with Request to Get Item => ${err}`)
-    } else {
-        data.Item ? console.log(`Fetching item... => ${JSON.stringify(data.Item)}`) : console.log(`Item not found in DynamoDB table`)
-    }
+        if (err) {
+            console.log(`Error with Request to Get Item => ${err}`);
+        } else {
+            data.Item ? console.log(`Fetching item... => ${JSON.stringify(data.Item)}`) : console.log(`Item not found in DynamoDB table`)
+        }
     }).promise();
 
     return item
