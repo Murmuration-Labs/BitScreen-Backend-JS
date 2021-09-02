3bba2eeaf18d6ad78571df169a127af8b5a7ba15
Alexandru Marian Constantin <alex@keyko.io>
2021-09-03 00:45:55 +0300

    BTSC-191 add provider column in the config table

--- src/router/config.ts
+++ src/router/config.ts
@@ -2,18 +2,33 @@ import * as express from 'express';
 import { Request, Response } from 'express';
 import { getRepository } from 'typeorm';
 import { Config } from '../entity/Settings';
+import { Provider } from '../entity/Provider';
 
 const configRouter = express.Router();
 
-configRouter.get('/', async (req: Request, res: Response) => {
-  const config = await getRepository(Config).findOne();
+configRouter.get('/:providerId', async (req: Request, res: Response) => {
+  const {
+    params: { providerId },
+  } = req;
+
+  // checks for both null and undefined
+  if (typeof providerId == null) {
+    return res.status(400).send({ message: 'Please provide a providerId.' });
+  }
+
+  const provider = await getRepository(Provider).findOne(providerId);
+  if (!provider) {
+    return res.status(404).send({});
+  }
+
+  const config = await getRepository(Config).findOne({
+    where: {
+      provider,
+    },
+  });
 
   if (!config) {
-    const newConfig = new Config();
-    newConfig.config = JSON.stringify(DEFAULT_CONFIG);
-
-    const dbConfig = await getRepository(Config).save(newConfig);
-    return res.send({ id: dbConfig.id, ...JSON.parse(dbConfig.config) });
+    return res.status(404).send({});
   }
 
   return res.send({ id: config.id, ...JSON.parse(config.config) });
@@ -21,18 +36,33 @@ configRouter.get('/', async (req: Request, res: Response) => {
 
 configRouter.put('/', async (req: Request, res: Response) => {
   const {
-    body: { id, ...config },
+    body: { providerId, ...config },
   } = req;
+
+  // checks for both null and undefined
+  if (typeof providerId == null) {
+    return res.status(400).send({ message: 'Please provide a providerId.' });
+  }
+
+  const provider = await getRepository(Provider).findOne(providerId);
+  if (!provider) {
+    return res.status(404).send({});
+  }
 
   if (!config) {
     return res.status(400).end({ message: 'Empty config now allowed.' });
   }
 
-  const existingConfig = await getRepository(Config).findOne();
+  const existingConfig = await getRepository(Config).findOne({
+    where: {
+      provider,
+    },
+  });
 
   if (!existingConfig) {
     const newConfig = new Config();
-    newConfig.config = JSON.stringify(config);
+    newConfig.provider = provider;
+    newConfig.config = JSON.stringify(DEFAULT_CONFIG);
 
     const dbConfig = await getRepository(Config).save(newConfig);
     return res.send({ id: dbConfig.id, ...JSON.parse(dbConfig.config) });
