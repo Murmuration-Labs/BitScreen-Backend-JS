7d0a5f967256005fb6ccf634bc191fccc2df0929
Stefan Manciu <manciustefan@gmail.com>
2021-09-30 17:20:37 +0300

    Fixed cid router and renamed controller

--- src/router/cid.ts
+++ src/router/cid.ts
@@ -1,10 +1,11 @@
 import * as express from 'express';
 import { verifyAccessToken } from '../service/jwt';
-import {cid_override, create_cid, delete_cid, move_cid} from "../controllers/cid.controller";
+import {cid_override, create_cid, delete_cid, edit_cid, move_cid} from "../controllers/cid.controller";
 
 const cidRouter = express.Router();
 
 cidRouter.post('/', verifyAccessToken, create_cid);
+cidRouter.put('/:id', verifyAccessToken, edit_cid);
 cidRouter.post('/:id/move/:toFilterId', verifyAccessToken, move_cid);
 cidRouter.get('/override', verifyAccessToken, cid_override);
 cidRouter.delete('/:id', verifyAccessToken, delete_cid);
