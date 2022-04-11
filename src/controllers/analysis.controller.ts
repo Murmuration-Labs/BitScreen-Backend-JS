f5810a08628ceaabb565244d90a92ee31a64bf2b
Stefan Manciu <manciustefan@gmail.com>
2022-04-11 15:04:10 +0300

    WIP analysis endpoints

---
+++ src/controllers/analysis.controller.ts
@@ -0,0 +1,5 @@
+import {Request, Response} from "express";
+
+export const save_analysis = async (req: Request, res: Response) => {
+  
+}
---
+++ src/router/analysis.ts
@@ -0,0 +1,5 @@
+import * as express from "express";
+
+const analysisRouter = express.Router();
+
+export default analysisRouter;
