f3684ded9af04f7ba66d7fd1e430b2ab14ae1a0b
Stefan Manciu <manciustefan@gmail.com>
2022-04-11 16:29:08 +0300

    Save analysis route

--- src/router/analysis.ts
+++ src/router/analysis.ts
@@ -1,5 +1,9 @@
 import * as express from "express";
+import cidRouter from "./cid";
+import {save_analysis} from "../controllers/analysis.controller";
 
 const analysisRouter = express.Router();
+
+cidRouter.post('/', save_analysis);
 
 export default analysisRouter;
