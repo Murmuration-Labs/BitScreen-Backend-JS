b8faeedda8673baacdce2e6bc23814b3e8eace86
ssallam <36411297+ssallam@users.noreply.github.com>
2022-08-15 12:59:37 +0200

    fix error in web3.storage service

--- src/service/web3storage.service.ts
+++ src/service/web3storage.service.ts
@@ -1,6 +1,6 @@
-import {Web3Storage} from "web3.storage";
+import { Service, Web3Storage } from "web3.storage"
 
-export const web3Storage = new Web3Storage({token: process.env.WEB3_STORAGE_TOKEN})
+export const web3Storage = new Web3Storage({token: process.env.WEB3_STORAGE_TOKEN} as Service)
 
 export const getDealsByCid = async (cid: string) => {
     const status = await web3Storage.status(cid);
