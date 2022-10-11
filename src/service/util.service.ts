b8305eacdf25cfcc0b28ef2a96ef1bbddf17fc70
Alexandru-Marian Constantin <alexm.constantin@gmail.com>
2022-10-11 16:47:01 +0300

    Modified the way account export is handled according to brief

--- src/service/util.service.ts
+++ src/service/util.service.ts
@@ -28,3 +28,7 @@ export const formatDate = (date) => new Date(date).toLocaleString('en-US', {
 });
 
 export const formatTime = (date) => new Date(date).toLocaleString('en-US', { hour: '2-digit', minute: '2-digit' });
+
+export const getEnumKeyFromValue = (enumKey: any, enumType: any) => {
+  return Object.keys(enumType)[Object.values(enumType).indexOf(enumKey)]
+}
