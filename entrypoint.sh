5a3984bde49cb01532cdb2331c7c65ec9a950154
ssallam <36411297+ssallam@users.noreply.github.com>
2022-11-30 10:45:07 +0100

    fix typeorm issues in deployment

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,5 @@
 #!/bin/sh
 
-cp package.json package.json.backup
-cp tsconfig.json tsconfig.json.backup
-
-cp typeorm-package.json package.json
-cp typeorm-tsconfig.json tsconfig.json
-
-yarn run schema:sync
-
-cp package.json.backup package.json
-cp tsconfig.json.backup tsconfig.json
+yarn run schema:synch
 
 pm2-runtime "./build/index.js" --node-args="--experimental-modules --es-module-specifier-resolution=node --loader ts-node/esm"
