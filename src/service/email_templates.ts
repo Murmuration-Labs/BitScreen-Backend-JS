3eda23a1f26b65236d5018b4ffeeb30e35bf3013
Alexandru-Marian Constantin <alexm.constantin@gmail.com>
2023-04-19 12:59:16 +0300

    fix: updated content of email (in sendgrid template) and the info sent to sendgrid

--- src/service/email_templates.ts
+++ src/service/email_templates.ts
@@ -3,9 +3,18 @@ import { bitscreenUri, lookingGlassUri } from '../config';
 import { Complaint } from '../entity/Complaint';
 
 export const CreateComplaint = {
-  subject: 'Thank you for submitting a complaint',
-  text: 'Thank you for submitting a complaint',
-  body: `<strong>Thank you for submitting a complaint</strong>`,
+  subject: 'Complaint Received - Filecoin Network Content',
+  text: `Thank you for submitting your complaint regarding content hosted on the Filecoin network. Your complaint will be reviewed by our assessors, and the included CIDs may be added to filter lists for storage providers to optionally subscribe and filter out known CIDs from storage and retrieval deals. We appreciate your efforts in maintaining the network's integrity.
+
+  Upon resolution of your complaint, you will receive a copy of the assessment by email, and a record will be published to our transparency hub.`,
+  body: `
+  <div>
+    Thank you for submitting your complaint regarding content hosted on the Filecoin network. Your complaint will be reviewed by our assessors, and the included CIDs may be added to filter lists for storage providers to optionally subscribe and filter out known CIDs from storage and retrieval deals. We appreciate your efforts in maintaining the network's integrity.
+  </div>
+  <br>
+  <div>
+    Upon resolution of your complaint, you will receive a copy of the assessment by email, and a record will be published to our transparency hub.
+  </div>`,
 };
 
 export const MarkAsSpam = {
@@ -28,7 +37,9 @@ export const Reviewed = {
     const bitscreenURI = bitscreenUri();
     const lookingGlassURI = lookingGlassUri();
     const assessor = {
-      name: complaint.assessor.provider.contactPerson,
+      name:
+        complaint.assessor.provider.businessName ||
+        complaint.assessor.provider.contactPerson,
       link: `${lookingGlassURI}/assessors/${complaint.assessor.id}`,
     };
 
